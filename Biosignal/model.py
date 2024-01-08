cb02c1681abbeb294eb2e6ab46723f6c187d64e3
Natthanich Hirunchavarod <108257658+nattntn@users.noreply.github.com>
2024-01-09 03:52:24 +0700

    Update model.py

--- Biosignal/model.py
+++ Biosignal/model.py
@@ -12,12 +12,12 @@ from logger import get_logger
 def simple_model():
     model = nn.Sequential(
         nn.Conv1d(in_channels=1, out_channels=128, kernel_size=50, stride=25, bias=False),
-        nn.BatchNorm1d(num_features=32, eps=0.001, momentum=0.01),
+        nn.BatchNorm1d(num_features=128, eps=0.001, momentum=0.01),
         nn.ReLU(inplace=True),
         nn.MaxPool1d(kernel_size=8, stride=8),
         nn.Dropout(p=0.5),
         nn.Conv1d(in_channels=128, out_channels=128, kernel_size=8, stride=1, bias=False),
-        nn.BatchNorm1d(num_features=64, eps=0.001, momentum=0.01),
+        nn.BatchNorm1d(num_features=128, eps=0.001, momentum=0.01),
         nn.ReLU(inplace=True),
         nn.MaxPool1d(kernel_size=8, stride=8),
         nn.Dropout(p=0.5),
